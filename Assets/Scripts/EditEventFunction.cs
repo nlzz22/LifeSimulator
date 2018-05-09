8f3266e28ad3d6c05e27aefe7da2b2c5bd80a59b
nlzz22 <nlzz_22@hotmail.com>
2018-05-10 00:01:46 +0800

    Editing of event function is now allowed.

---
+++ Assets/Scripts/EditEventFunction.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditEventFunction : MonoBehaviour {
+    private GameObject currentEventFunctionWhole;
+    private GameObject functionsGrid;
+
+    private GameObject FindCurrentEventFunctionWhole()
+    {
+        if (currentEventFunctionWhole == null)
+        {
+            currentEventFunctionWhole = transform.parent.parent.gameObject;
+        }
+
+        return currentEventFunctionWhole;
+    }
+
+    private GameObject FindFunctionsGrid()
+    {
+        if (functionsGrid == null)
+        {
+            GameObject currEventFuncWhole = FindCurrentEventFunctionWhole();
+            functionsGrid = currEventFuncWhole.transform.parent.gameObject;
+        }
+
+        return functionsGrid;
+    }
+
+	public void Edit()
+    {
+        FindFunctionsGrid();
+
+        // disable all event function whole.
+        int numChild = functionsGrid.transform.childCount;
+        for (int i = 0; i < numChild; i++)
+        {
+            functionsGrid.transform.GetChild(i).gameObject.SetActive(false);
+        }
+        // enable only the current event function whole.
+        currentEventFunctionWhole.SetActive(true);
+
+        // swap over the view from rep to input view.
+        transform.parent.gameObject.SetActive(false); // hide rep.
+        currentEventFunctionWhole.transform.Find("EventFunctionInput").gameObject.SetActive(true); // show input.
+
+    }
+}
