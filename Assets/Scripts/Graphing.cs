fd335da9bd695bbe2929bae47da7cc1fe75c9a17
nlzz22 <nlzz_22@hotmail.com>
2018-05-12 01:24:21 +0800

    Added graphing function to find line eqn given 2 points

---
+++ Assets/Scripts/Graphing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graphing : MonoBehaviour {
+    // Finds the equation of a line, given 2 points.
+    // Returns false for invalid points.
+	public static bool FindLineEquation(float x1, float y1, float x2, float y2, out float gradient, out float constant)
+    {
+        float numerator = y2 - y1;
+        float denominator = x2 - x1;
+        if (denominator == 0)
+        {
+            // dummy values.
+            gradient = 0;
+            constant = 0;
+            return false;
+        }
+
+        gradient = numerator / denominator;
+        constant = y1 - gradient * x1;
+        return true;
+    }
+}
